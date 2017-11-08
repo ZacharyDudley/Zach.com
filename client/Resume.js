b876cae252d968b897605e1469ac5a812a9d95ec
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-08 08:31:36 -0600

    some styling

--- client/Resume.js
+++ client/Resume.js
@@ -5,40 +5,36 @@ const Resume = () => {
   return (
     <div className="contentBox">
       <div className="section">
-        <div id="skills">
-          <h2>SKILLS</h2>
-            <h3>Proficient:</h3>
-            <p>Creative Problem Solving, Scrum and Agile Methodologies</p>
-            <h3>Knowledgable:</h3>
-            <p>JavaScript, HTML/CSS, SQL, C#, Ruby, Lua</p>
-            <h3>Some experience:</h3>
-            <p>React, Redux</p>
-            <p>Unity3D, Adobe Creative Suite, Microsoft Office, Inkscape, Blender, Pro Tools</p>
+        <h2>SKILLS</h2>
+          <h3>Proficient:</h3>
+          <p>Creative Problem Solving, Scrum and Agile Methodologies</p>
+          <h3>Knowledgable:</h3>
+          <p>JavaScript, HTML/CSS, SQL, C#, Ruby, Lua</p>
+          <h3>Some experience:</h3>
+          <p>React, Redux</p>
+          <p>Unity3D, Adobe Creative Suite, Microsoft Office, Inkscape, Blender, Pro Tools</p>
 
-            <h3>Languages:</h3>
-              <p>JavaScript, HTML, CSS, SQL, C#, Ruby, Lua</p>
-            <h3>Libraries:</h3>
-              <p>React, Redux, Express, Socket.io, PostgreSQL, Webpack, Bootstrap, jQuery, Jasmine, Mocha, Chai</p>
-            <h3>Software:</h3>
-              <p>Unity3D, Adobe Creative Suite, Microsoft Office, Inkscape, Blender, Pro Tools</p>
-        </div>
+          <h3>Languages:</h3>
+            <p>JavaScript, HTML, CSS, SQL, C#, Ruby, Lua</p>
+          <h3>Libraries:</h3>
+            <p>React, Redux, Express, Socket.io, PostgreSQL, Webpack, Bootstrap, jQuery, Jasmine, Mocha, Chai</p>
+          <h3>Software:</h3>
+            <p>Unity3D, Adobe Creative Suite, Microsoft Office, Inkscape, Blender, Pro Tools</p>
       </div>
 
       <div className="section">
-          <div id="education">
-              <h2>EDUCATION</h2>
-                  <h2>Miami University  <small><i>Oxford, OH</i></small> 2008</h2>
-                    <p>Bachelor of Arts: Philosophy</p>
+        <h2>EDUCATION</h2>
+            <h2>Miami University  <small><i>Oxford, OH</i></small> 2008</h2>
+              <p>Bachelor of Arts: Philosophy</p>
 
-                  <h2>University of Akron  <small><i>Akron, OH</i></small> 2012</h2>
-                    <p>Courses towards Masters in English</p>
+            <h2>University of Akron  <small><i>Akron, OH</i></small> 2012</h2>
+              <p>Courses towards Masters in English</p>
 
-                  <h2>Milwaukee Area Technical College  <small><i>Milwaukee, WI</i></small> 2017</h2>
-                    <p>Associate of Applied Science: Computer Simulation and Gaming</p>
+            <h2>Milwaukee Area Technical College  <small><i>Milwaukee, WI</i></small> 2017</h2>
+              <p>Associate of Applied Science: Computer Simulation and Gaming</p>
 
-                  <h2>Fullstack Academy  <small><i>Chicago, IL</i></small> 2017</h2>
-                    <p>Programming experience</p>
-          </div>
+            <h2>Fullstack Academy  <small><i>Chicago, IL</i></small> 2017</h2>
+              <p>Programming experience</p>
       </div>
 
     </div>
