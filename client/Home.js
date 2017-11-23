011549947aa792b543466d27b425d5b2e1e53ff5
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2017-11-23 13:57:49 -0600

    WIP

--- client/Home.js
+++ client/Home.js
@@ -5,16 +5,12 @@ class Home extends Component {
     super()
     this.state = {
       names: [
-        'DESIGNER', 'DEVELOPER', 'PHILOSOPHER',
-        'SOLIPSIST', 'MINIMALIST', 'POET',
-        'MENSCH', 'ARTIST', 'CONSUMER',
-        'AMERICAN', 'PROGRAMMER', 'COMEDIAN',
-        'GUITARIST', 'READER', 'OHIOAN',
-        'DREAMER', 'FRIEND', 'CITIZEN',
-        'MATERIALIST', 'IDEALIST', 'HUMAN',
-        'GEOCENTRIST', 'BIBLIOPHILE', 'CAT FANCIER',
-        'DADAIST', '\'PATAPHYSICIST', 'POSTMODERNIST',
-        'CODER', 'MILLENIAL', 'ARIES',
+        'DESIGNER', 'DEVELOPER', 'PHILOSOPHER', 'SOLIPSIST', 'MINIMALIST',
+        'POET', 'MENSCH', 'ARTIST', 'CONSUMER', 'AMERICAN',
+        'PROGRAMMER', 'COMEDIAN', 'GUITARIST', 'READER', 'OHIOAN',
+        'DREAMER', 'FRIEND', 'CITIZEN', 'MATERIALIST', 'IDEALIST',
+        'HUMAN', 'GEOCENTRIST', 'BIBLIOPHILE', 'CAT FANCIER', 'DADAIST',
+        '\'PATAPHYSICIST', 'POSTMODERNIST', 'CODER', 'MILLENIAL', 'ARIES',
         'EVERYMAN', 'GENTLEMAN', 'OMNIVORE'
       ],
       nameOne: 'DEVELOPER',
@@ -26,88 +22,74 @@ class Home extends Component {
     }
   }
 
+  getRandomTime() {
+    let randomTime = Math.floor(Math.random() * 4) + 1
+    return randomTime * 1000
+  }
+
+  getRandomName() {
+    let randomName = Math.floor(Math.random() * this.state.names.length)
+    let name = this.state.names[randomName]
+    return name
+  }
+
+  getUniqueName() {
+    let name = ''
+
+    while (name === '' || name === this.state.nameOne || name === this.state.nameTwo || name === this.state.nameThree) {
+      name = this.getRandomName()
+    }
+
+    return name
+  }
+
+
   nameOpacity(element, inOrOut) {
     let nameElement = document.getElementById(element)
-    console.log(nameElement)
-
+    nameElement.classList.remove('fadeIn')
+    nameElement.classList.remove('fadeOut')
 
     if (inOrOut === 'in') {
-      nameElement.classList.remove('fadeOut')
       nameElement.classList.add('fadeIn')
     } else {
-      nameElement.classList.remove('fadeIn')
       nameElement.classList.add('fadeOut')
     }
   }
 
-  setName(whichName) {
+  setNameAndTime(whichName) {
     let hasNewName = false
 
     while (!hasNewName) {
-      let randomName = Math.floor(Math.random() * this.state.names.length)
-      let name = this.state.names[randomName]
+      let name = this.getUniqueName()
 
       if (name !== this.state.nameOne && name !== this.state.nameTwo && name !== this.state.nameThree) {
         if (whichName === 'one') {
-          let time = this.setTime()
-          this.setState({nameOne: name})
-          this.setState({timeOne: time})
+          let time = this.getRandomTime()
+          this.setState({nameOne: name, timeOne: time})
           this.nameOpacity('one', 'in')
         } else if (whichName === 'two') {
-          let time = this.setTime()
-          this.setState({nameTwo: name})
-          this.setState({timeTwo: time})
+          let time = this.getRandomTime()
+          this.setState({nameTwo: name, timeTwo: time})
           this.nameOpacity('two', 'in')
         } else if (whichName === 'three') {
-          let time = this.setTime()
-          this.setState({nameThree: name})
-          this.setState({timeThree: time})
+          let time = this.getRandomTime()
+          this.setState({nameThree: name, timeThree: time})
           this.nameOpacity('three', 'in')
         }
         hasNewName = true
       }
     }
-
-
-    const chooseAndAnimateName = () => {
-      let time = this.setTime()
-
-      window.requestAnimationFrame(chooseAndAnimateName)
-    }
-
-    // let randomName = Math.floor(Math.random() * this.state.names.length)
-    // let name = this.state.names[randomName]
-    // while (name !== this.state.nameOne && name !== this.state.nameTwo && name !== this.state.nameThree){
-    //   if (whichName === 'one') {
-    //     let time = this.setTime()
-    //     this.setState({nameOne: name})
-    //     this.setState({timeOne: time})
-    //   } else if (whichName === 'two') {
-    //     let time = this.setTime()
-    //     this.setState({nameTwo: name})
-    //     this.setState({timeTwo: time})
-    //   } else if (whichName === 'three') {
-    //     let time = this.setTime()
-    //     this.setState({nameThree: name})
-    //     this.setState({timeThree: time})
-    //   }
-    // }
-  }
-
-  setTime() {
-    let randomTime = Math.floor(Math.random() * 4) + 1
-    return randomTime * 1000
   }
 
   componentDidMount() {
     this.displayOne = setInterval(
-      () => this.setName('one'), this.state.timeOne)
+      () => this.setNameAndTime('one'), this.state.timeOne)
 
     this.displayTwo = setInterval(
-      () => this.setName('two'), this.state.timeTwo)
+      () => this.setNameAndTime('two'), this.state.timeTwo)
 
     this.displayThree = setInterval(
-      () => this.setName('three'), this.state.timeThree)
+      () => this.setNameAndTime('three'), this.state.timeThree)
   }
 
   componentWillUnmount() {
