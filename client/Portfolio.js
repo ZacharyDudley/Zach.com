0c169423b513aa9c1eb27be72bf49999e15bc313
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-01-30 12:19:28 -0500

    added ChessBall and changed some roles

--- client/Portfolio.js
+++ client/Portfolio.js
@@ -5,9 +5,15 @@ const Portfolio = () => {
   return (
     <div className="contentBox">
       <div className="section">
+        <h2>ChessBall | <small><a href="https://github.com/ZacharyDudley/chessball">git repository</a></small></h2>
+        <p>A two player turn-based game combining mechanics of Chess and Soccer.</p>
+        <p>ROLE: Developer</p>
+      </div>
+
+      <div className="section">
         <h2>Domination | <small><a href="https://domination-squad.herokuapp.com/">domination-squad.herokuapp.com</a></small></h2>
         <p>An in-browser recreation of the classic board game Risk with multiplayer (player v player) or single player (player v AI) capabilities.</p>
-        <p>ROLE: Developer</p>
+        <p>ROLE: Designed GUIs </p>
 
         <div className="videoBox">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/51-RLBMWXgQ?rel=0" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
@@ -18,12 +24,12 @@ const Portfolio = () => {
       <div className="section">
         <h2>Bento's Bazaar | <small><a href="https://bentos-bazaar.herokuapp.com/">bentos-bazaar.herokuapp.com</a></small></h2>
           <p>A speculative e-commerce site specializing in pet supplies</p>
-          <p>ROLE: Developer</p>
+          <p>ROLE: Developed page layouts</p>
       </div>
 
       <div className="section">
         <h2>Walker Talker | <small><a href="https://github.com/ZacharyDudley/walk-and-talk-2">git repository</a></small></h2>
-          <p>A walkie-talkie that streams real-time audio over the internet</p>
+          <p>An attempt to create walkie-talkie that streams real-time audio over the internet</p>
           <p>ROLE: Developer</p>
       </div>
 
