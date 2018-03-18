5e4aae4a8a78fe1b6e0302ea6a48088f72bcf871
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-03-18 08:59:44 -0500

    added photo flex box and screenshots for bento and domination

--- client/Portfolio.js
+++ client/Portfolio.js
@@ -19,7 +19,14 @@ const Portfolio = () => {
       <div className="section">
         <h2>Domination | <small><a href="https://domination-squad.herokuapp.com/">domination-squad.herokuapp.com</a></small></h2>
         <p>An in-browser recreation of the classic board game Risk with multiplayer (player v player) or single player (player v AI) capabilities.</p>
-        <p>ROLE: Designed GUIs </p>
+        <p>ROLE: Designed GUIs</p>
+
+        <div className="pictureBox">
+          <img src="/screenShots/domination/dominationGameBoard.png" alt="Game board" />
+          <img src="/screenShots/domination/dominationTutorial.png" alt="Tutorial" />
+          <img src="/screenShots/domination/dominationNewGame.png" alt="Creating game" />
+          <img src="/screenShots/domination/dominationLobby.png" alt="Joining game" />
+        </div>
 
         <div className="videoBox">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/51-RLBMWXgQ?rel=0" frameBorder="0" gesture="media" allow="encrypted-media" allowFullScreen></iframe>
@@ -31,6 +38,13 @@ const Portfolio = () => {
         <h2>Bento's Bazaar | <small><a href="https://bentos-bazaar.herokuapp.com/">bentos-bazaar.herokuapp.com</a></small></h2>
           <p>A speculative e-commerce site specializing in pet supplies</p>
           <p>ROLE: Developed page layouts</p>
+
+          <div className="pictureBox">
+            <img src="/screenShots/bento/bentoProducts.png" alt="All products" />
+            <img src="/screenShots/bento/bentoCollar.png" alt="Product page" />
+            <img src="/screenShots/bento/bentoCart.png" alt="Cart" />
+        </div>
+
       </div>
 
       <div className="section">
