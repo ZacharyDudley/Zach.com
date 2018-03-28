8d753547f4a3d18a58f60631eb34e5164113b438
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-03-28 09:20:38 -0500

    added experience to resume with mild formatting

--- client/Resume.js
+++ client/Resume.js
@@ -1,22 +1,40 @@
 import React from 'react'
 
 const Resume = () => {
-
   return (
     <div className="contentBox">
       <div className="section">
         <h2>SKILLS</h2>
+        <hr />
           <h3>Proficient:</h3>
-            <p>JavaScript, Node, React, Redux, HTML, CSS, Express, Git, Agile, Scrum, Unity3D</p>
+            <p>JavaScript, Node, React, Redux, HTML, CSS, Git, Agile, Unity3D</p>
           <h3>Knowledgable:</h3>
-            <p>Socket.io, Websocket, PostgreSQL, Firebase, SQL, Webpack, C#, Adobe CS, Blender, GIMP, Inkscape, Pro Tools</p>
+            <p>Express, Socket.io, Websocket, React Native, Webpack, Jest, Jasmine, PostgreSQL, SQL, Firebase, C#, Adobe Creative Suite, Blender, Inkscape, Pro Tools</p>
           <h3>Some experience:</h3>
-            <p>Sass, Bootstrap, Bluebird, Jasmine, Mocha, Chai, jQuery, Ruby, Lua</p>
+            <p>Sass, Bootstrap, Bluebird, Mocha, Chai, jQuery, Heroku, Lua, Ruby, Xcode</p>
+      </div>
+
+      <div className="section">
+        <h2>EXPERIENCE</h2>
+        <hr />
+          <h2>Freelance Developer | <small>Team ETHERNAUTS</small></h2>
+            <h3>2018</h3>
+            <p>Contributed front-end code to the blockchain game ETHERNAUTS</p>
+
+          <h2>Designer | <small>Forever Interactive</small></h2>
+            <h3>2017</h3>
+            <p>Designed and refined mechanics for the game Visions of Zosimos</p>
+
+          <h2>QA Tester | <small>iBeta Quality Assurance</small></h2>
+            <h3>2011</h3>
+            <p>Worked with a team to test pre-release consumer software</p>
+
       </div>
 
       <div className="section">
         <h2>EDUCATION</h2>
-            <h2>Fullstack Academy of Code | <small>Chicago, IL</small></h2>
+        <hr />
+          <h2>Fullstack Academy of Code | <small>Chicago, IL</small></h2>
               <h3>2017</h3>
               <p>Software Engineering Immersive</p>
 
